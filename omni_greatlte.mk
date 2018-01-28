46824cc546e1738b48f96ccaaaa48bccd65ce46d
ananjaser1211 <ananjaser@gmail.com>
2018-01-28 22:35:44 +0400

    Init tree.
    Thanks to @jesec @geiti94 @tkkg194 for their S8/N8 Tree source.

---
+++ omni_greatlte.mk
@@ -0,0 +1,18 @@
+# Release name
+PRODUCT_RELEASE_NAME := greatlte
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_PACKAGES += \
+	charger_res_images \
+	charger
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := greatlte
+PRODUCT_NAME := omni_greatlte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
