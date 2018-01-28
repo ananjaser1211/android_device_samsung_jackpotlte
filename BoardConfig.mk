eedcb306ce43edaa38341f9b1429abc6753b30d8
ananjaser1211 <ananjaser@gmail.com>
2018-01-29 00:19:43 +0400

    Adjust Boardconfig and add Flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,9 +1,11 @@
 # Platform
 BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := exynos5
-TARGET_SOC := exynos8895
-TARGET_BOOTLOADER_BOARD_NAME := universal8895
+TARGET_SOC := exynos7885
+TARGET_BOOTLOADER_BOARD_NAME := universal7885
+TARGET_BOARD_PLATFORM_GPU := mali-g71
 TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := universal7885
 TARGET_NO_RADIOIMAGE := true
 
 # Architecture
@@ -24,36 +26,53 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_USES_UNCOMPRESSED_KERNEL := true
+TARGET_PREBUILT_KERNEL := device/samsung/jackpotlte/Image
 
 # Image
+BOARD_KERNEL_CMDLINE :=
 BOARD_CUSTOM_BOOTIMG_MK :=  device/samsung/jackpotlte/bootimg.mk
-TARGET_PREBUILT_KERNEL := device/samsung/jackpotlte/Image
 TARGET_PREBUILT_DTB := device/samsung/jackpotlte/dt.img
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SRPQC03B001KU
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SRPQH21C001RU
 
-# File systems
+# Use this flag if the board has a ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
+TW_HAS_MTP := true
 RECOVERY_SDCARD_ON_DATA := true
-TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
-TW_MAX_BRIGHTNESS := 36600
-TW_DEFAULT_BRIGHTNESS := 16200
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 150
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
-TW_INCLUDE_NTFS_3G := true
+TW_MTP_DEVICE := "/dev/mtp_usb"
 TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_NTFS_3G := true
 TW_EXTRA_LANGUAGES := true
 TW_USE_NEW_MINADBD := true
+TWRP_INCLUDE_LOGCAT := true
+
+# Color fix
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 
 # Include
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/jackpotlte/include
 
 # SELinux Policies
 BOARD_SEPOLICY_DIRS := device/samsung/jackpotlte/sepolicy
+
+# Encryption support
+TW_INCLUDE_CRYPTO := true
+#TW_INCLUDE_CRYPTO_SAMSUNG := true
+#TARGET_HW_DISK_ENCRYPTION := true
+
+# Debug flags
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+TWRP_EVENT_LOGGING := false
